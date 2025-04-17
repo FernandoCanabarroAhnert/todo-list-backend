788569f44b87d3472cadf6ea225a7b3aa5dda3c2
FernandoCanabarroAhnert <ahnertfernando499@gmail.com>
2025-04-17 12:05:07 -0300

    :wrench: fix: Configurando CORS para aceitar o domínio do front-end

--- src/main/java/com/test/todo_list_backend/security/ProjectSecurityConfig.java
+++ src/main/java/com/test/todo_list_backend/security/ProjectSecurityConfig.java
@@ -51,7 +51,7 @@ public class ProjectSecurityConfig {
                 @Override
                 public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                    config.setAllowedOrigins(Collections.singletonList("https://app.fernandocanabarrodev.tech"));
                     config.setAllowedMethods(Collections.singletonList("*"));
                     config.setAllowedHeaders(Collections.singletonList("*"));
                     config.setAllowCredentials(true);
